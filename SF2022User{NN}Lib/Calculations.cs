506f8d60bd8c163c713cc9ca0e52e79d629d432c
Vip_Ghost <lied00@mail.ru>
2023-03-21 10:27:11 +0300

    Добавьте файлы проекта.

---
+++ SF2022User{NN}Lib/Calculations.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF2022User_NN_Lib
+{
+    public class Calculations
+    {
+        public static string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+            if (startTimes == null)
+            {
+                return null;
+            }
+            if (durations == null)
+            {
+                return null;
+            }
+            if (beginWorkingTime == null)
+            {
+                return null;
+            }
+            if (endWorkingTime == null)
+            {
+                return null;
+            }
+            TimeSpan[,] periods = new TimeSpan[startTimes.Length, 2];
+            if (startTimes.Length == durations.Length)
+            {
+                for (int i = 0; i < startTimes.Length; i++)
+                {
+                    periods[i, 0] = startTimes[i];
+                    periods[i, 1] = startTimes[i].Add(new TimeSpan(0, durations[i], 0));
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+            TimeSpan[] start = new TimeSpan[0];
+            TimeSpan[] end = new TimeSpan[0];
+            int j = 0;
+            for (TimeSpan i = beginWorkingTime; i <= endWorkingTime; i = i.Add(new TimeSpan(0, consultationTime, 0)))
+            {
+                if (i == endWorkingTime)
+                {
+                    if (i.Subtract(new TimeSpan(0, consultationTime, 0)) == periods[j, 1])
+                    {
+                        Array.Resize(ref start, start.Length + 1);
+                        Array.Resize(ref end, end.Length + 1);
+                        start[start.Length - 1] = i.Subtract(new TimeSpan(0, consultationTime, 0));
+                        end[end.Length - 1] = i;
+                    }
+                }
+                else if (i < periods[j, 0])
+                {
+                    if (periods[j, 0] < i.Add(new TimeSpan(0, consultationTime, 0)) && i.Add(new TimeSpan(0, consultationTime, 0)) < periods[j, 1])
+                    {
+                        i = periods[j, 1];
+                        continue;
+                    }
+                    Array.Resize(ref start, start.Length + 1);
+                    Array.Resize(ref end, end.Length + 1);
+                    start[start.Length - 1] = i;
+                    end[end.Length - 1] = i.Add(new TimeSpan(0, consultationTime, 0));
+                }
+                else
+                {
+                    if (i == periods[j, 1])
+                    {
+                        Array.Resize(ref start, start.Length + 1);
+                        Array.Resize(ref end, end.Length + 1);
+                        start[start.Length - 1] = i;
+                        end[end.Length - 1] = i.Add(new TimeSpan(0, consultationTime, 0));
+                        j++;
+                        continue;
+                    }
+                    i = periods[j, 1];
+                    if (j != periods.GetLength(0) - 1 && periods[j, 1] == periods[j + 1, 0])
+                    {
+                        i = periods[j + 1, 1];
+                        Array.Resize(ref start, start.Length + 1);
+                        Array.Resize(ref end, end.Length + 1);
+                        start[start.Length - 1] = i;
+                        end[end.Length - 1] = i.Add(new TimeSpan(0, consultationTime, 0));
+                    }
+                    j++;
+                }
+            }
+
+            string[] result = new string[0];
+            for (int i = 0; i < start.Length; i++)
+            {
+                Array.Resize(ref result, result.Length + 1);
+                result[i] = $"{start[i].ToString(@"hh\:mm")}-{end[i].ToString(@"hh\:mm")}";
+            }
+            return result;
+        }
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SF2022User_NN_Lib;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_actualIsStringArray_true()
+        {
+            TimeSpan[] st = new TimeSpan[5];
+            int[] d = new int[] { 60, 30, 10, 10, 40 };
+            st[0] = new TimeSpan(10, 0, 0);
+            st[1] = new TimeSpan(11, 0, 0);
+            st[2] = new TimeSpan(15, 0, 0);
+            st[3] = new TimeSpan(15, 30, 0);
+            st[4] = new TimeSpan(16, 50, 0);
+            TimeSpan begin = new TimeSpan(8, 0, 0), end = new TimeSpan(18, 0, 0);
+            int cons = 30;
+
+            string[] actual = Calculations.AvailablePeriods(st, d, begin, end, cons);
+
+            Assert.IsInstanceOfType(actual, typeof(string[]));
+        }
+
+        [TestMethod]
+        public void TestMethod_isBeginLessEnd_true()
+        {
+            TimeSpan[] st = new TimeSpan[5];
+            int[] d = new int[] { 60, 30, 10, 10, 40 };
+            st[0] = new TimeSpan(10, 0, 0);
+            st[1] = new TimeSpan(11, 0, 0);
+            st[2] = new TimeSpan(15, 0, 0);
+            st[3] = new TimeSpan(15, 30, 0);
+            st[4] = new TimeSpan(16, 50, 0);
+            TimeSpan begin = new TimeSpan(8, 0, 0), end = new TimeSpan(18, 0, 0);
+            int cons = 30;
+
+            string[] actual = Calculations.AvailablePeriods(st, d, begin, end, cons);
+
+            Assert.IsTrue(begin < end);
+        }
+
+        [TestMethod]
+        public void TestMethod_actualIsNull_true()
+        {
+            TimeSpan[] st = new TimeSpan[] { };
+            int[] d = null;
+            TimeSpan begin = new TimeSpan(), end = new TimeSpan(18, 0, 0);
+            int cons = 30;
+
+            string[] actual = Calculations.AvailablePeriods(st, d, begin, end, cons);
+
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void TestMethod_actualIsNotEqualExpected_false()
+        {
+            string[] expected = new string[] { "08:00-08:30", "08:30-09:00", "09:00-09:30", "09:30-10:00", "11:30-12:00", "12:00-12:30", "12:30-13:00", "13:00-13:30", "13:30-14:00", "14:00-14:30", "14:30-15:00", "15:40-16:10", "16:10-16:40", "17:30-18:00" };
+            TimeSpan[] st = new TimeSpan[6];
+            int[] d = new int[] { 120, 90, 60, 20, 30, 30 };
+            st[0] = new TimeSpan(8, 0, 0);
+            st[1] = new TimeSpan(11, 0, 0);
+            st[2] = new TimeSpan(15, 0, 0);
+            st[3] = new TimeSpan(16, 40, 0);
+            st[4] = new TimeSpan(18, 0, 0);
+            st[5] = new TimeSpan(19, 30, 0);
+            TimeSpan begin = new TimeSpan(8, 0, 0), end = new TimeSpan(20, 0, 0);
+            int cons = 30;
+
+            string[] actual = Calculations.AvailablePeriods(st, d, begin, end, cons);
+
+            Assert.AreNotEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_actualIsNotNull_false()
+        {
+            TimeSpan[] st = new TimeSpan[5];
+            int[] d = new int[] { 60, 30, 10, 10, 40 };
+            st[0] = new TimeSpan(10, 0, 0);
+            st[1] = new TimeSpan(11, 0, 0);
+            st[2] = new TimeSpan(15, 0, 0);
+            st[3] = new TimeSpan(15, 30, 0);
+            st[4] = new TimeSpan(16, 50, 0);
+            TimeSpan begin = new TimeSpan(8, 0, 0), end = new TimeSpan(18, 0, 0);
+            int cons = 100;
+
+            string[] actual = Calculations.AvailablePeriods(st, d, begin, end, cons);
+
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
+        public void TestMethod_isNullStartTimesThrowsException_true()
+        {
+            TimeSpan[] st = null;
+            int[] d = new int[] { 60, 30, 10, 10, 40 };
+            TimeSpan begin = new TimeSpan(8, 0, 0), end = new TimeSpan(18, 0, 0);
+            int cons = 100;
+
+            string[] actual = Calculations.AvailablePeriods(st, d, begin, end, cons);
+
+            Assert.ThrowsException<AssertFailedException>(() => Assert.ThrowsException<SystemException>(() => Calculations.AvailablePeriods(st, d, begin, end, cons)));
+        }
+
+        [TestMethod]
+        public void TestMethod_isDifferentArrayLengthThrowsException_true()
+        {
+            TimeSpan[] st = new TimeSpan[5];
+            int[] d = new int[] { 60, 30, 10, 10, 40, 40 };
+            st[0] = new TimeSpan(10, 0, 0);
+            st[1] = new TimeSpan(11, 0, 0);
+            st[2] = new TimeSpan(15, 0, 0);
+            st[3] = new TimeSpan(15, 30, 0);
+            st[4] = new TimeSpan(16, 50, 0);
+            TimeSpan begin = new TimeSpan(8, 0, 0), end = new TimeSpan(18, 0, 0);
+            int cons = 100;
+
+            Assert.ThrowsException<AssertFailedException>(() => Assert.ThrowsException<IndexOutOfRangeException>(() => Calculations.AvailablePeriods(st, d, begin, end, cons)));
+        }
+
+        [TestMethod]
+        public void TestMethod_isNullValuesThrowsException_true()
+        {
+            TimeSpan[] st = null;
+            int[] d = null;
+            TimeSpan begin = new TimeSpan(), end = new TimeSpan();
+            int cons = 0;
+
+            Assert.ThrowsException<AssertFailedException>(() => Assert.ThrowsException<IndexOutOfRangeException>(() => Calculations.AvailablePeriods(st, d, begin, end, cons)));
+        }
+
+        [TestMethod]
+        public void TestMethod_isActualNotNullWithNegativeValues_false()
+        {
+            TimeSpan[] st = new TimeSpan[5];
+            int[] d = new int[] { -60, -30, -10, -10, -40 };
+            st[0] = new TimeSpan(-10, 0, 0);
+            st[1] = new TimeSpan(-11, 0, 0);
+            st[2] = new TimeSpan(-15, 0, 0);
+            st[3] = new TimeSpan(-15, 30, 0);
+            st[4] = new TimeSpan(-16, 50, 0);
+            TimeSpan begin = new TimeSpan(-8, 0, 0), end = new TimeSpan(-18, 0, 0);
+            int cons = -100;
+
+            string[] actual = Calculations.AvailablePeriods(st, d, begin, end, cons);
+
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
+        public void TestMethod_isBoundaryConditionsThrowsException_true()
+        {
+            TimeSpan[] st = new TimeSpan[5];
+            int[] d = new int[] { 60, 30, 10, 10, 40 };
+            st[0] = new TimeSpan(9, 0, 0);
+            st[1] = new TimeSpan(1, 0, 0);
+            st[2] = new TimeSpan(15, 0, 0);
+            st[3] = new TimeSpan(15, 30, 0);
+            st[4] = new TimeSpan(16, 50, 0);
+            TimeSpan begin = new TimeSpan(8, 0, 0), end = new TimeSpan(18, 0, 0);
+            int cons = int.MaxValue;
+
+            Assert.ThrowsException<AssertFailedException>(() => Assert.ThrowsException<ArgumentOutOfRangeException>(() => Calculations.AvailablePeriods(st, d, begin, end, cons)));
+        }
+    }
+}
